fa0bea526de25667a2e73cf5ec2e1b7268d51b28
4D4850 <84929757+4D4850@users.noreply.github.com>
2021-08-15 15:42:32 -0500

    Update DEUT.cpp
    still fixing syntax

--- src/simulation/elements/DEUT.cpp
+++ src/simulation/elements/DEUT.cpp
@@ -53,7 +53,7 @@ void Element::Element_DEUT()
 
 static int update(UPDATE_FUNC_ARGS)
 {
-	int r, rx, ry, trade, np;
+	int r, rx, ry, rt, trade, np;
 	float gravtot = fabs(sim->gravy[(y/CELL)*(XRES/CELL)+(x/CELL)])+fabs(sim->gravx[(y/CELL)*(XRES/CELL)+(x/CELL)]);
 	// Prevent division by 0
 	float temp = std::max(1.0f, (parts[i].temp + 1));
@@ -63,7 +63,6 @@ static int update(UPDATE_FUNC_ARGS)
 	// Compress when Newtonian gravity is applied
 	// multiplier=1 when gravtot=0, multiplier -> 5 as gravtot -> inf
 	maxlife = maxlife*int(5.0f - 8.0f/(gravtot+2.0f));
-	int r,rx,ry,rt;
 	for (rx=-1; rx<2; rx++)
 		for (ry=-1; ry<2; ry++)
 			rt = TYP(r);
