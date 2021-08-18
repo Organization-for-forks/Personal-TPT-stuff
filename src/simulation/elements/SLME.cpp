0b694923b0d269fb4da048b7725adde098d81487
4D4850 <84929757+4D4850@users.noreply.github.com>
2021-08-18 12:42:49 -0500

    Update SLME.cpp
    Gutted

--- src/simulation/elements/SLME.cpp
+++ src/simulation/elements/SLME.cpp
@@ -1,1 +1,99 @@
+#include "simulation/ElementCommon.h"
 
+int Element_FIRE_update(UPDATE_FUNC_ARGS);
+static int update(UPDATE_FUNC_ARGS);
+static int graphics(GRAPHICS_FUNC_ARGS);
+static void create(ELEMENT_CREATE_FUNC_ARGS);
+
+void Element::Element_PHOT()
+{
+	Identifier = "DEFAULT_PT_PHOT";
+	Name = "PHOT";
+	Colour = PIXPACK(0xFFFFFF);
+	MenuVisible = 1;
+	MenuSection = SC_NUCLEAR;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 1.00f;
+	Loss = 1.00f;
+	Collision = -0.99f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+
+	Weight = -1;
+
+	DefaultProperties.temp = R_TEMP + 900.0f + 273.15f;
+	HeatConduct = 251;
+	Description = "Photons. Refracts through glass, scattered by quartz, and color-changed by different elements. Ignites flammable materials.";
+
+	Properties = TYPE_ENERGY|PROP_LIFE_DEC|PROP_LIFE_KILL_DEC;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+	DefaultProperties.life = 680;
+	DefaultProperties.ctype = 0x3FFFFFFF;
+
+	Update = &update;
+	Graphics = &graphics;
+	Create = &create;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+	
+}
+
+static int graphics(GRAPHICS_FUNC_ARGS)
+{
+	int x = 0;
+	*colr = *colg = *colb = 0;
+	for (x=0; x<12; x++) {
+		*colr += (cpart->ctype >> (x+18)) & 1;
+		*colb += (cpart->ctype >>  x)     & 1;
+	}
+	for (x=0; x<12; x++)
+		*colg += (cpart->ctype >> (x+9))  & 1;
+	x = 624/(*colr+*colg+*colb+1);
+	*colr *= x;
+	*colg *= x;
+	*colb *= x;
+
+	*firea = 100;
+	*firer = *colr;
+	*fireg = *colg;
+	*fireb = *colb;
+
+	*pixel_mode &= ~PMODE_FLAT;
+	*pixel_mode |= FIRE_ADD | PMODE_ADD | NO_DECO;
+	if (cpart->flags & FLAG_PHOTDECO)
+	{
+		*pixel_mode &= ~NO_DECO;
+	}
+	return 0;
+}
+
+static void create(ELEMENT_CREATE_FUNC_ARGS)
+{
+	float a = RNG::Ref().between(0, 7) * 0.78540f;
+	sim->parts[i].vx = 3.0f * cosf(a);
+	sim->parts[i].vy = 3.0f * sinf(a);
+	int Element_FILT_interactWavelengths(Particle* cpart, int origWl);
+	if (TYP(sim->pmap[y][x]) == PT_FILT)
+		sim->parts[i].ctype = Element_FILT_interactWavelengths(&sim->parts[ID(sim->pmap[y][x])], sim->parts[i].ctype);
+}
